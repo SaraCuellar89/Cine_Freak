c01e2eb935e8059b8d253f561cb548723aeca3c3
SaraCuellar89 <saracuellar213@gmail.com>
2025-09-30 21:32:43 -0500

    Update Encabezado.jsx

--- src/Componentes/Encabezado.jsx
+++ src/Componentes/Encabezado.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 const Encabezado = () => {
 
-    const [sesion, setSesion] = useState({})
+    const [sesion, setSesion] = useState(null)
 
     useEffect(() => {
         const obtener_info_sesion = async () => {
@@ -38,33 +38,35 @@ const Encabezado = () => {
             <p>Freak</p>
             <div>
                 {/* Si el usuario es un administrador no muestra el boton inicio, de resto si */}
-                {sesion ? 
-                (sesion.rol === 'Administrador' ? 
-                    null :
-                    (<Link to='/'><p>Inicio</p></Link>)
-                ) :
-                (<Link to='/'><p>Inicio</p></Link>)}
+                {!sesion || sesion.rol !== 'Administrador' ? (
+                  <Link to='/'><p>Inicio</p></Link>
+                ) : null}
                 
                 {/* Si el usuario es un administrador muestra el panel de control de las funciiones */}
-                {sesion ? 
-                (sesion.rol === 'Administrador' ? 
-                    (<Link to='/Funciones_Adminitrador'><p>Funciones</p></Link>) :
-                    <Link to='/Funciones'><p>Funciones</p></Link>
-                ) :
-                (<Link to='/Funciones'><p>Funciones</p></Link>)}
+                {sesion ? (
+                    sesion.rol === 'Administrador' ? (
+                        <Link to='/Funciones_Administrador'><p>Funciones</p></Link>
+                    ) : (
+                        <Link to='/Funciones'><p>Funciones</p></Link>
+                    )
+                ) : (
+                    <Link to='/Inicio_Sesion'><p>Iniciar Sesión</p></Link>
+                )}
 
 
                 <Link><p>Menu</p></Link>
 
 
                 {/* Cambiar link del logo dependiendo de si se ha iniciado sesion y de quien lo ha hecho */}
-                {sesion ? 
-                (sesion.rol === 'Usuario' ? 
-                    (<Link to='/Perfil_Usuario'><img src={logo} alt="logo" /></Link>) :
-                    (<Link to='/Perfil_Administrador'><img src={logo} alt="logo" /></Link>)
-                ) :
-                <Link to='/Inicio_Sesion'><img src={logo} alt="logo" /></Link>
-                }
+                {sesion ? (
+                    sesion.rol === 'Usuario' ? (
+                        <Link to='/Perfil_Usuario'><img src={logo} alt="logo" /></Link>
+                    ) : (
+                        <Link to='/Perfil_Administrador'><img src={logo} alt="logo" /></Link>
+                    )
+                ) : (
+                    <Link to='/Inicio_Sesion'><img src={logo} alt="logo" /></Link>
+                )}
             </div>
         </div>
     )
